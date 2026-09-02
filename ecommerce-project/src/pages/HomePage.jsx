0b1153ef2df3b71e5cde3b29d4fdfc8ad675e21c
codeKobby <6.291801e+07+codeKobby@users.noreply>
2026-09-02 11:28:08 +0000

    feat: refactor header implementation and replace links with react-router in Checkout, Home, Orders, and Tracking pages

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,38 +1,11 @@
-import { Link } from "react-router";
-import "./header.css"
-import "./HomePage.css"
+import { Header } from "../components/Header";
+
+import "./HomePage.css";
 
 export function HomePage() {
   return (
     <>
-      <div className='header'>
-        <div className='left-section'>
-          <Link to='/' className='header-link'>
-            <img className='logo' src='images/logo-white.png' />
-            <img className='mobile-logo' src='images/mobile-logo-white.png' />
-          </Link>
-        </div>
-
-        <div className='middle-section'>
-          <input className='search-bar' type='text' placeholder='Search' />
-
-          <button className='search-button'>
-            <img className='search-icon' src='images/icons/search-icon.png' />
-          </button>
-        </div>
-
-        <div className='right-section'>
-          <a className='orders-link header-link' href='/orders'>
-            <span className='orders-text'>Orders</span>
-          </a>
-
-          <Link className='cart-link header-link' to='/checkout'>
-            <img className='cart-icon' src='images/icons/cart-icon.png' />
-            <div className='cart-quantity'>3</div>
-            <div className='cart-text'>Cart</div>
-          </Link>
-        </div>
-      </div>
+      <Header />
 
       <div className='home-page'>
         <div className='products-grid'>
